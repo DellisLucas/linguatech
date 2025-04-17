db7dccb64e249c17d48455c595e2f5fa9cadf0d7
DellisLucas <lucasdellis1@gmail.com>
2025-04-17 10:48:19 -0300

    feat(backend): adiciona modelo de usuário

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app import db
+from .user import User
+from .module import Module
+from .category import Category
+from .question import Question, Option
+from .user_progress import UserProgress
+from .user_placement_answer import UserPlacementAnswer
