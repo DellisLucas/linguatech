d81f120bc126687915fa38b42f686d900f9e9859
DellisLucas <lucasdellis1@gmail.com>
2025-04-17 10:48:19 -0300

    feat(backend): configura estrutura base do Flask

---
+++ backend/app/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+from app.config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Atualizar CORS para usar a variável de ambiente
+    CORS(app, resources={r"/api/*": {"origins": app.config['FRONTEND_URL']}}, supports_credentials=True)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Importa modelos para registrar no SQLAlchemy
+    from app import models
+
+    # Blueprints
+    from app.routes.auth import auth_bp
+    from app.routes.questions import questions_bp
+    from app.routes.modules import modules_bp
+    from app.routes.category import categories_bp
+    from app.routes.leveling import placement_bp
+    from app.routes.explainerAI import explainer_bp
+
+
+
+    app.register_blueprint(explainer_bp)
+    app.register_blueprint(placement_bp)
+    app.register_blueprint(questions_bp)
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    app.register_blueprint(modules_bp, url_prefix='/api')
+    app.register_blueprint(categories_bp, url_prefix='/api')
+
+    @app.before_request
+    def handle_options():
+        if request.method == "OPTIONS":
+            return '', 200
+
+    with app.app_context():
+        db.create_all()
+
+    return app
